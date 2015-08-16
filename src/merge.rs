TheJakeSchmidt/wikipedia-minus-wikipedians

commit 00a02d473df6201a1e56a940f23f1b1aefd998d9

In cases of true merge conflicts, keep the vandalized content rather than the possibly-clean content.

src/merge.rs
@@ -190,7 +190,7 @@ pub fn try_merge(old: &str, new: &str, other: &str, marker: &str) -> (String, bo
         old_words.clone(), other_words.clone());
     let (new_lcs, other_lcs) = match (new_lcs, other_lcs) {
         (Some(new_lcs), Some(other_lcs)) => (new_lcs, other_lcs),
-        _ => { return (new.to_owned(), true); },
+        _ => { info!("Timed out computing LCS"); return (new.to_owned(), true); },
     };
 
     let mut bytes = Vec::<u8>::new();
@@ -242,8 +242,14 @@ pub fn try_merge(old: &str, new: &str, other: &str, marker: &str) -> (String, bo
                     // Truly conflicting
                     // In a normal 3-way merge program, this means a failed merge requiring user
                     // intervention. Since we have no user to intervene and want to keep as much
-                    // vandalism as possible, we keep new_chunk here so we can keep going.
-                    bytes.extend(new_chunk);
+                    // vandalism as possible, we keep other_chunk here and keep going.
+                    bytes.extend(::START_MARKER.as_bytes());
+                    bytes.extend(marker.as_bytes());
+                    bytes.extend(::START_MARKER.as_bytes());
+                    bytes.extend(other_chunk);
+                    bytes.extend(::END_MARKER.as_bytes());
+                    bytes.extend(marker.as_bytes());
+                    bytes.extend(::END_MARKER.as_bytes());
                 }
             },
         }

src/main.rs
@@ -93,6 +93,8 @@ mod wiki;
 
 // TODO: consider doing s/en.wikipedia.org/this app's url/ on the HTML before serving it. This
 // currently works fine, but might not over HTTPS.
+// TODO: add performance-tuning flags: Size at which we skip diffs, number of consecutive timeouts
+// to tolerate, timeout on a single diff.
 
 /// Given a list of (section title, section content) pairs, spawns one thread for each section. Each
 /// thread accepts (clean content, vandalized content, revision ID) tuples for its section over a

commit 05464cad1b56586705bc5060cc5b9b7ee92201f5

Fix merge::tests::test_try_merge_conflicting(), which I broke in commit 00a02d4.

src/merge.rs
@@ -461,9 +461,10 @@ mod tests {
         let old = "First sentence. Second sentence.";
         let new = "First sentence. Second sentence changed one way.";
         let other = "First sentence changed. Second sentence changed a different way.";
-        let expected =
-            format!("First {}123{}sentence changed. {}123{}Second sentence changed one way.",
-                    ::START_MARKER, ::START_MARKER, ::END_MARKER, ::END_MARKER);
+        let expected = format!(
+            "First {}123{}sentence changed. {}123{}Second {}123{}sentence changed a different way.{}123{}",
+            ::START_MARKER, ::START_MARKER, ::END_MARKER, ::END_MARKER,
+            ::START_MARKER, ::START_MARKER, ::END_MARKER, ::END_MARKER);
         assert_eq!((expected, false), try_merge(old, new, other, "123"));
     }
 